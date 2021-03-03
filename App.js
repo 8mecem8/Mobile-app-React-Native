1ae2f6ea102857f5390ebc64471544da2f0446a4
Cem  Onal <42888722+8mecem8@users.noreply.github.com>
2021-03-02 22:06:09 -0500

    a

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+<script src="http://localhost:8097"></script>
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+console.log( ' this is from outside of component')
 export default function App() {
   return (
+    
     <View style={styles.container}>
       <Text>This is a Test write to see how codes are working</Text>
       <StatusBar style="auto" />
